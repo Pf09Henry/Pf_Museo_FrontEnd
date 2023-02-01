0ce0fd339662481faabb6fb8d4329c0f46f47163
Erika <erikaladner5@gmail.com>
2023-02-01 18:49:35 -0300

    creacion de login/register/carrousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Layout from './Components/Layout/Layout';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
     </div>
   );
