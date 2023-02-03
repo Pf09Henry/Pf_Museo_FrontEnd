dbdeba25c66285b670092a7823730593fccd5930
Erika <erikaladner5@gmail.com>
2023-02-03 17:34:29 -0300

    Actualizar App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
-
 import Exhibits from './Components/Exhibits/Exhibits';
 import Details from './Components/Details/Details';
-
-
-
+import AreaPaleo from './Components/areaPaleo/areaPaleo';
+import AreaBio from './Components/areaBio/areaBio';
+import AreaEducation from './Components/areaEducation/areaEducation';
+import Culture from './Components/Activities/Culture/Culture'
+import Holidays from './Components/Activities/Holidays/Holidays'
+import Night from "./Components/Activities/Night/Night";
 import Layout from './Components/Layout/Layout';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import Nav from './Components/Nav/Nav';
+import Historia from './Components/Historia/Historia';
+import Crear from './Components/FormCreacion/CrearEvento';
 
 function App() {
   return (
@@ -24,6 +28,14 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/exhibits' element={<Exhibits />} />
         <Route path='/details' element={<Details />} />
+        <Route path='/areaPaleo' element={<AreaPaleo />} />
+        <Route path='/areaBio' element={<AreaBio />} />
+        <Route path='/areaEducation' element={<AreaEducation />} />
+        <Route path='/culture' element={<Culture />} />
+        <Route path='/holidays' element={<Holidays />} />
+        <Route path='/night' element={<Night />} />
+        <Route path='/history' element={<Historia/>} />
+        <Route path='/create-activity' element={<Crear/>} />
       </Routes>
       <Footer />
     </div>
