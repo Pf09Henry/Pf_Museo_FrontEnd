144d2f2861cfe49e7492679a6936204d7f0f3fb7
Erika <erikaladner5@gmail.com>
2023-02-16 11:25:44 -0300

    detalles filtros y searchbar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './SearchBar.css';
 import { getEventsName } from '../../Actions/AppActions/appActions';
@@ -16,17 +16,22 @@ export default function SearchBar() {
         // console.log(name)
     }
 
-    function handleSubmit(e) {
+  /*   function handleSubmit(e) {
         e.preventDefault()
         dispatch(getEventsName(name))
         //setName("")     no borra lo escrito (para facilidad del usuario)
-    }
+    } */
+
+
+  useEffect(() => {
+        dispatch(getEventsName(name));
+      }, [dispatch,name]);
 
     return (
         <div className='sbcontainer'>
-            <form onSubmit={e => { handleSubmit(e) }}></form>
-            <Input className='sbinput' onChange={(e) => handleInputChange(e)} type='text' placeholder='buscar evento...' value={name} />
-            <button class="btn btn-outline-success btn-search" onClick={(e) => handleSubmit(e)} type='submit'>Buscar</button>
+   {/*          <form onSubmit={e => { handleSubmit(e) }}></form> */}
+           <p className='buscar-serchbar'>Buscar</p> <Input className='sbinput' onChange={(e) => handleInputChange(e)} type='text' placeholder='Nombre del evento...' value={name} />
+       {/*     <button class="btn btn-outline-success btn-search" onClick={(e) => handleSubmit(e)} type='submit'>Buscar</button>  */}
         </div>
     )
 }   
