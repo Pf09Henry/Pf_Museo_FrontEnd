0929d280ba532e6580412ae53e25c68de0635989
eldeividrig <davidgabrielb@hotmail.com>
2023-02-23 20:42:02 -0300

    pypal terminado

--- src/Components/Paypal/PayPal.js
+++ src/Components/Paypal/PayPal.js
@@ -4,76 +4,77 @@ import { CartContext } from '../../Context/index';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getEvents, postTicket,putEvent, postMail } from '../../Actions/AppActions/appActions';
+import { getEvents, postTicket, putEvent, postMail } from '../../Actions/AppActions/appActions';
 import Swal from 'sweetalert2';
 
 
 
 export default function Paypal() {
     const { user } = useAuth0();
-    const usuario = useSelector((state)=> state.users)
-    const eventos = useSelector((state)=> state.eventos)
+    const usuario = useSelector((state) => state.users)
+    const eventos = useSelector((state) => state.eventos)
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { products, saveProducts } = useContext(CartContext);
 
-
-    function findUserID (name){
-        let findUser = usuario.filter((e)=> e.name === user.name)
-         console.log("USUARIO",findUser[0].id) 
+    //esto es para detectar el user que hace la compra
+    function findUserID(name) {
+        let findUser = usuario.filter((e) => e.name === user.name)
+        console.log("USUARIO", findUser[0].id)
         return findUser[0].id
     }
 
-  
+
     let info = [];
     let acount = [];
     let amount = [];
     let idEvent = [];
+    // recopila info del carrito 
 
-
-
+    let total = 0
     for (let i = 0; i < products.length; i++) {
         info.push(products[i].name)
         acount.push(products[i].price)
         amount.push(products[i].cantidad)
-        idEvent.push(products[i].id)        
+        idEvent.push(products[i].id)
+        total = total + (acount[i] * amount[i])
     }
-    let total = acount*amount
-/*     console.log("PRODUCTOS",products)
-    console.log("INFO",info)
-    console.log("ACOUNT",acount)
-    console.log("AMOUNT",amount)
-    console.log(idEvent)
+    console.log("esto es total! :", total)
 
-    console.log("TOTAL",total) */
+
 
     const paypal = useRef();
-   
-        const ticket = {
+    let ticket = []
+    for (let i = 0; i < products.length; i++) {
+        ticket.push({
             methodOfPurchase: 'PayPal' || '',
-            amount: parseInt(amount[0]) || '',
-            totalOfPurchase: total,
-            eventId: idEvent[0] || '',
+            amount: parseInt(amount[i]) || '',
+            totalOfPurchase: acount[i] * amount[i],
+            eventId: idEvent[i] || '',
             userId: findUserID(user.name) || '',
-        }
+        })
+    }
+
+    console.log("ide evento ", idEvent)
+    console.log("ticket", ticket)
 
 
 
-        function findEventCupos (id){
-            let findEvent = eventos.filter((e)=> e.id === id)
-          
-           return findEvent[0].availability
-           
-        }
+    function findEventCupos(id) {
+        let findEvent = eventos.filter((e) => e.id === id)
 
-        const putEvento = {
-            availability: findEventCupos(idEvent[0]) - parseInt(amount[0]),
-        }
-       /*  console.log('Aqui los :', ticket) */
-      
+        return (findEvent[0].availability)
+
+    }
+    // let id = "52db0c75-febb-417f-a2a1-142c9e3e72a8"
+    // console.log(findEventCupos(id))
 
 
-        useEffect(() => {
+    /*  console.log('Aqui los :', ticket) */
+
+
+
+    useEffect(() => {
         window.paypal.Buttons({
             createOrder: (data, actions, err) => {
                 /* console.log("1: ", data)
@@ -90,7 +91,7 @@ export default function Paypal() {
                         },
                     ]
                 })
-               /*  console.log("esto es order: ", order) */
+                /*  console.log("esto es order: ", order) */
                 return order
             },
             onCancel: (data) => {
@@ -99,21 +100,21 @@ export default function Paypal() {
                     text: 'Pago cancelado',
                     icon: 'warning',
                     confirmButtonText: 'Ok'
-                  })
-             
+                })
+
                 console.log(data)
             },
             onApprove: async (data, actions) => {
                 /* console.log("actions: ", actions) */
                 const order = await actions.order.capture();
-               /*  console.log("esto es order: ", order) */
+                /*  console.log("esto es order: ", order) */
                 if (order) {
                     Swal.fire({
                         title: 'Ok',
                         text: 'Tu Pago fue efectuado con Exito',
                         icon: 'success',
                         confirmButtonText: 'OK'
-                      })
+                    })
                     // axios.post("http://localhost:3001/send_email", {
                     //     mail: user.email,
                     //     subject: "Pago realizado con Exito!",
@@ -121,21 +122,32 @@ export default function Paypal() {
                     // })
                     // dispatch(postTicket(valuesId))
                     // console.log('Aqui post', postTicket)
-                    let datosEmail = {
-                        mail: user.email,
-                        subject: "Pago Paypal",
-                        message: "Su compra fue concretada de manera exitosa!",
-                        ticket: ticket
+
+                    for (let i = 0; i < ticket.length; i++) {
+                        let cupos = findEventCupos(idEvent[i]) - parseInt(amount[i])
+                        const putEvento = {
+                            availability: cupos,
+                        }
+                        console.log(findEventCupos(idEvent[i]))
+                        console.log("cupos: ", cupos)
+                        console.log(putEvento)
+                        let datosEmail = {
+                            mail: user.email,
+                            subject: "Pago Paypal",
+                            message: "Su compra fue concretada de manera exitosa!",
+                            ticket: ticket[i]
+                        }
+                        dispatch(postTicket(ticket[i]))
+                        dispatch(postMail(datosEmail))
+                        dispatch(putEvent(putEvento, idEvent[i]))
                     }
-                    dispatch(postTicket(ticket))
-                    dispatch(postMail(datosEmail))
-                    dispatch(putEvent(putEvento, idEvent[0]))
-                  /*   console.log('Aqui post', postTicket) */
-                      
+
+                    /*   console.log('Aqui post', postTicket) */
+
                     saveProducts([])
                 }
                 navigate('/eventos')
-           /*      console.log(data) */
+                /*      console.log(data) */
                 //>>aca podemos accionar cualquier lógica necesaria que le indique al user que todo funcionó<<
             },
             onError: (err) => {
