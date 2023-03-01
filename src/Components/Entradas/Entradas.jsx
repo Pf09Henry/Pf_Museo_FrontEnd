d386f1054bccfb220577900b53b7bd7f67d9ae6d
JheisonVR <daviid.vargasr@gmail.com>
2023-03-01 16:38:18 -0500

    modificacion de estilos entradas card flip con precios

--- src/Components/Entradas/Entradas.jsx
+++ src/Components/Entradas/Entradas.jsx
@@ -54,40 +54,103 @@ export default function Entradas() {
 
     return (
         <main>
-            <div className="container px-4 py-5" id="custom-cards">
-                <h2 className="pb-2 border-bottom">Entradas</h2>
+            <div className="container-fluid px-4 py-1" id="custom-cards">
+                
+                <h2 className="pb-2 my-4 fw-bold display-5 text-success ">Entradas</h2>
 
-                <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-                    <div className="col">
-                        <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg firstCardEntrys ">
-                            <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                                <img src={logo} alt="Logo" width="32" height="32" className="rounded-circle border border-white" />
-                                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Martes a Viernes</h3>
+                {/* <div className="row row-cols-1 row-cols-md-3 align-items-center g-4 py-5"> */}
+                <div className="d-grid h-100 align-items-center">
+                <div className='row' >                
+                    <div className="col flipCardEntry">
+                            <div className="flip-card-entry-inner shadow ">
+                                <div className="flip-card-entry-front">
+                                    <img src={logo} alt="Logo" width="32" height="32" className="rounded-circle border border-white my-1" />
+                                    <img src='/src/Imagenes/Pictures_Biologia/BiologicaPataLargarto.jpg' alt='' />
+                                    <h3 className="mt-3 mb-4 display-6 fw-bold text-light">Semana</h3>
+                                </div>
+                                <div className=' container  flip-card-entry-back' >
+                                    <h4 className='pt-4 mt-3 mb-4 display-6 fw-bold text-dark' >Valor: $ 500</h4>
+                                    <h4 className="pt-2 mt-1 mb-4  lh-1 fw-bold text-dark">Martes a Viernes</h4>
+                                    <div className='container overflow-hidden' >
+                                        {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: pata, id: "entry-week", name: "Entrada Semana", price: 500, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
+                                    </div>
+                                </div>
+                                
                             </div>
-                            {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: pata, id: "entry-week", name: "Entrada Semana", price: 500, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
-                            
+                        </div>
+
+
+
+
+                    <div className="col flipCardEntry">
+                        <div className="flip-card-entry-inner shadow ">
+                            <div className="flip-card-entry-front-dos">
+                                <img src={logo} alt="Logo" width="32" height="32" className="rounded-circle border border-white my-1" />
+                                <h3 className="mt-3 mb-4 display-6 fw-bold text-dark">Fin de Semana</h3>
+                            </div>
+                            <div className=' flip-card-entry-back-dos' >
+                                <h4 className='pt-4 mt-3 mb-4 display-6 fw-bold text-dark'>Valor: $ 800</h4>
+                                <h4 className="pt-2 mt-1 mb-4 lh-1 fw-bold text-dark">Sábado y Domingo</h4>
+                                <div className='container overflow-hidden' >
+                                    {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: pata, id: "entry-week", name: "Entrada Semana", price: 500, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
+                                </div>
+                            </div>                           
                         </div>
                     </div>
 
-                    <div className="col">
-                        <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg secondCardEntrys">
-                            <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                                <img src={logo} alt="Logo" width="32" height="32" className="rounded-circle border border-white" />
-                                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold text-dark">Fin de Semana</h3>
+                    <div className="col flipCardEntry">
+                        <div className="flip-card-entry-inner shadow ">
+                            <div className="flip-card-entry-front-tres">
+                                <img src={logo} alt="Logo" width="32" height="32" className="rounded-circle border border-white my-1" />
+                                <h3 className="mt-3 mb-4 display-6 fw-bold text-light">Vacaciones</h3>
                             </div>
-                            {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: lagarto, id: "entry-weekend", name: "Entrada Fin de Semana", price: 900, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
+                            <div className=' flip-card-entry-back-tres' >
+                                <h4 className='pt-4 mt-3 mb-5 display-6 fw-bold text-light'>Valor: $ 900</h4>
+                                {/* <h4 className="pt-2 mt-1 mb-4 display-6 lh-1 fw-bold text-light ">Sábado y Domingo</h4> */}
+                                <div className='container overflow-hidden' >
+                                    {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: pata, id: "entry-week", name: "Entrada Semana", price: 500, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
+                                </div>
+                            </div>                           
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg thirdCardEntrys">
-                            <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                                <img src={logo} alt="Logo" width="32" height="32" className="rounded-circle border border-white" />
-                                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Vacaciones de Verano</h3>
+
+
+
+
+                    {/* <div className="col flipCardEntry">
+                        <div className="flip-card-entry-inner shadow">
+                            <div className='flip-card-entry-front-tres'>
+                                <img src={logo} alt="Logo" width="32" height="32" className="" />
+                                <h3 className="">Vacaciones de Verano</h3>
                             </div>
-                            {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: buho, id: "entry-vacation", name: "Entrada Vacaciones", price: 800, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
+                            <div className='flip-card-entry-back-tres' >
+                                <h4 className=''>Valor: $ 900</h4>
+                                <h4 className="">Vacaciones</h4>
+                                {isAuthenticated ? <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }} onClick={() => handleAddToCart({ img: pata, id: "entry-week", name: "Entrada Semana", price: 500, cantidad: 1, fecha:date })} >Agregar al Carrito</Button> : <Button type="primary" style={{ backgroundColor: "rgb(56, 102, 103" }}  onClick={clickAuth} >Agregar al Carrito</Button>}
+                            </div>  
                         </div>
+                    </div> */}
                     </div>
                 </div>
+
+
+
+{/* 
+                <div class="flip-card">
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <img src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/07/Luffy-png.png?fit=800%2C800&ssl=1" alt="Avatar" style={{width:'300px',height:'300px'}}/>
+                        </div>
+                        <div class="flip-card-back">
+                            <h1 className='text-light fw-bold'>Luffy</h1>
+                            <p className='text-light fw-bold'>Rey de los Piratas</p>
+                            <p className='text-light fw-bold'>Hombre de Goma</p>
+                        </div>
+                    </div>
+                </div>  */}
+
+
+
                 <div className='btn-ext'>
                     <div className='btn-events'>
                         <Link to='/eventos'>
@@ -106,6 +169,7 @@ export default function Entradas() {
                     </div>
                 </div>
             </div>
+
         </main>
     )
 }
