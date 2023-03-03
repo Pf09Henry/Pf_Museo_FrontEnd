0c887abbda363f8ceffc71b4b4127dfbcba3cd31
BrayanMen <brayanjmr880@gmail.com>
2023-03-03 03:21:56 -0300

    Filtros/Nosotros/paginado

---
+++ src/Components/Sobre Nosotros/AboutUs.jsx
@@ -0,0 +1,235 @@
+import React from 'react'
+import reactnode from './ReactNode.png'
+import JsHtml from './JsHtmlCss.png'
+import Express from './express.png'
+import postgres from './postgre.png'
+import antd from './antD.png'
+import sequelize from './sequelize.png'
+import bootstrap from './bootstrap.png'
+
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import "./AboutUs.css"
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h2>Nuestro Equipo</h2>
+            <div className='containerA'>
+                <div>
+                    <div className='cardAbout card3'>
+                        <div>
+                            <h4 className='nameA'>Erika Noelia Ladner</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                </div>
+                                <div>
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                            <h6 className='nameA'>Contacto</h6>
+                            <div className='iconsA'>
+                                <a href='https://www.linkedin.com/in/erikaladner/'><BsLinkedin className='buttonGitLink' /></a>
+                                <a href='https://github.com/erikalad'><BsGithub className='buttonGitLink' /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card1'>
+                        <div>
+                            <h4 className='nameA'>Dario Balmaceda </h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                </div>
+                                <div>
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='https://www.linkedin.com/in/dario-balmaceda-/'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/dbandres'><BsGithub className='buttonGitLink' /></a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card2'>
+                        <div>
+                            <h4 className='nameA'>Gabriel Pérez</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                </div>
+                                <div>
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='https://www.linkedin.com/in/gabriel-p%C3%A9rez-52557534/'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/gabiperez'><BsGithub className='buttonGitLink' /></a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card4'>
+                        <div>
+                            <h4 className='nameA'>David Gabriel Barboza</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                </div>
+                                <div>
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='https://www.linkedin.com/in/david-barboza-6a4702170/'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/eldeividrig'><BsGithub className='buttonGitLink' /></a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card5'>
+                        <div>
+                            <h4 className='nameA'>Jheyson Vargas</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                </div>
+                                <div>
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='https://www.linkedin.com/in/jheison-david-vargas-rayo/'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/JheisonVR'><BsGithub className='buttonGitLink' /></a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card0'>
+                        <div>
+                            <h4 className='nameA'>Brayan Mendoza</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                </div>
+                                <div>
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='https://www.linkedin.com/in/brayan-jose-mendoza-rondon-52625a160/'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/BrayanMen'><BsGithub className='buttonGitLink' /></a>
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card6'>
+                        <div>
+                            <h4 className='nameA'>Emiliano Villa</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                </div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='https://www.linkedin.com/in/emiliano-villa-686364241/'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/e0212'><BsGithub className='buttonGitLink' /></a>
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className='cardAbout card7'>
+                        <div>
+                            <h4 className='nameA'>Alan Delgado</h4>
+                            <h5 className='nameA'>Aptitudes</h5>
+                            <div className='aptitudes'>
+                                <div><img src={JsHtml} width='180px' height='70px' alt="js" /></div>
+                                <div><img src={reactnode} width='120px' height='60px' alt="ReactNode" /></div>
+                                <div>
+                                    <img src={bootstrap} width='60px' height='60px' alt="bootstrap" />
+                                    <img src={Express} width='60px' height='60px' alt="Express" />
+                                </div>
+                                <div>
+                                    <img src={antd} width='60px' height='60px' alt="antd" />
+                                    <img src={sequelize} width='60px' height='60px' alt="sequelize" />
+                                    <img src={postgres} width='60px' height='60px' alt="postgres" />
+                                </div>
+                            </div>
+                        </div>
+                        <h6 className='nameA'>Contacto</h6>
+                        <div className='iconsA'>
+                            <a href='http://www.linkedin.com/in/alan-delgado8'><BsLinkedin className='buttonGitLink' /></a>
+                            <a href='https://github.com/DelgadoAlan'><BsGithub className='buttonGitLink' /></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="/contacto">
+                <button className='buttonContacto'><h3 className='h3'>Contactanos</h3></button>
+            </a>
+        </div>
+    )
+}
